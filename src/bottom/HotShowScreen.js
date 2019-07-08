49aabc0dc724faa3b7423a9da007f44243dd9e1e
黄宝杰 <huangbaojie0327@gmail.com>
2019-07-08 11:06:10 +0800

    完成列表，正在添加刷新

--- src/bottom/HotShowScreen.js
+++ src/bottom/HotShowScreen.js
@@ -1,30 +1,51 @@
 import React, {Component} from 'react';
-import {Image, StyleSheet, View, Text, FlatList, Platform, Dimensions, TouchableOpacity} from 'react-native';
+import {
+    Image,
+    StyleSheet,
+    View,
+    Text,
+    FlatList,
+    Platform,
+    Dimensions,
+    TouchableOpacity,
+    Alert,
+    ActivityIndicator,
+    Animated,
+    Easing,
+
+
+} from 'react-native';
 import CardView from '../utils/CardView';
+import {Rating, Button} from 'react-native-elements';
+import SmartRefresh from '../utils/SmartRefresh';
 
 
 /**
  * 热映
  */
 
-const URL = "https://api.douban.com/v2/movie/in_theaters";
+const url = "https://api.douban.com/v2/movie/in_theaters";
+const params={apikey: '0df993c66c0c636e29ecbb5344252a4a', city: '北京', start: '0', count: '100'};
 
 export default class HotShowScreen extends Component {
 
     constructor(props) {
         super(props);
+
         this.state = {
             productData: [],
+            prArrowDeg: new Animated.Value(0),
         };
+        this.base64Icon = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAB4AAABQBAMAAAD8TNiNAAAAJ1BMVEUAAACqqqplZWVnZ2doaGhqampoaGhpaWlnZ2dmZmZlZWVmZmZnZ2duD78kAAAADHRSTlMAA6CYqZOlnI+Kg/B86E+1AAAAhklEQVQ4y+2LvQ3CQAxGLSHEBSg8AAX0jECTnhFosgcjZKr8StE3VHz5EkeRMkF0rzk/P58k9rgOW78j+TE99OoeKpEbCvcPVDJ0OvsJ9bQs6Jxs26h5HCrlr9w8vi8zHphfmI0fcvO/ZXJG8wDzcvDFO2Y/AJj9ADE7gXmlxFMIyVpJ7DECzC9J2EC2ECAAAAAASUVORK5CYII=';
 
         // 在ES6中，如果在自定义的函数里使用了this关键字，则需要对其进行“绑定”操作，否则this的指向会变为空
         // 像下面这行代码一样，在constructor中使用bind是其中一种做法（还有一些其他做法，如使用箭头函数等）
-        this._fetchMovieData = this._fetchMovieData.bind(this);
+     //   this.onFetch = this.onFetch.bind(this);
     }
 
 
     componentDidMount() {
-        this._fetchMovieData(URL, {apikey: '0df993c66c0c636e29ecbb5344252a4a', city: '北京', start: '0', count: '100'});
+      //  this.onFetch(URL, {apikey: '0df993c66c0c636e29ecbb5344252a4a', city: '北京', start: '0', count: '100'});
     }
 
 
@@ -34,7 +55,7 @@ export default class HotShowScreen extends Component {
      * @param params
      * @private
      */
-    _fetchMovieData(url, params) {
+    async onFetch(page, pageSize, callback) {
 
         // 组装参数
         if (params) {
@@ -58,48 +79,179 @@ export default class HotShowScreen extends Component {
 
         })
 
-
     }
+
+     renderMovieData({item}) {
+        // 主演
+        let mainCast = item.casts.map(cast => cast.name).join(' / ');
+        // 观看人数
+        var total_count;
+        let count = item.collect_count;
+        if (count.toString().length < 5) {
+            total_count = count;
+        } else {
+            total_count = Math.floor(parseFloat(count) / 10000 * 10) / 10 + '万';
+        }
+
+        return (
+            <View style={styles.container}>
+
+                <CardView style={styles.cardStyle}>
+                    <View style={styles.movieContainer}>
+                        <Image style={styles.productImage}
+                               source={{uri: item.images.medium}}/>
+
+                        <View style={styles.movieContentContainer}>
+                            <View style={styles.contentLeft}>
+                                <Text style={styles.movieTitle}>{item.title}</Text>
+                                <Rating
+                                    style={{marginTop: 10}}
+                                    imageSize={15}
+                                    readonly
+                                    fractions={1}
+                                    startingValue={item.rating.average / 2}
+                                    ratingCount={5}/>
+                                <Text style={{marginTop: 10, fontSize: 11}}>导演： {item.directors[0].name}</Text>
+                                <Text style={{marginTop: 10, fontSize: 11}}>主演： {mainCast}</Text>
+                            </View>
+
+                            <View style={styles.contentRight}>
+                                <Text style={{fontSize: 9}}>{total_count}人看过</Text>
+                                <Button
+                                    buttonStyle={{
+                                        marginTop: 15,
+                                        width: 50,
+                                        height: 25,
+                                        borderColor: 'red',
+                                        borderWidth: 1
+                                    }}
+                                    title={'购票'}
+                                    titleStyle={{fontSize: 12, color: 'red'}}
+                                    type='outline'
+                                    onPress={() => Alert.alert('购票', null, null)}/>
+                            </View>
+
+
+                        </View>
+
+                    </View>
+                </CardView>
+
+            </View>
+
+
+        );
+    }
+
+    footerView() {
+        let footerLoading = () => (<View style={styles.footer}><ActivityIndicator animating={true} size="small" /><View style={{ marginLeft: 20 }}><Text style={{ textAlign: 'center' }}>正在加载中...</Text></View></View>);
+        let loadingNoMore = () => <View style={styles.footer}><Text style={{ textAlign: 'center' }}>已经到底了</Text></View>;
+        let loadingMore = (_getMore) =>
+            <TouchableOpacity style={styles.footer} onPress={() => _getMore()} >
+                <Text style={{ textAlign: 'center' }}>加载更多</Text>
+        </TouchableOpacity> ;
+
+        let noDataView = (_rePostFetch) => <View>
+            <Text style={{ textAlign: 'center' }}>没有数据</Text>
+            <TouchableOpacity
+
+                onPress={() => _rePostFetch()}
+            >
+                <Text style={{ color: '#333', textAlign: 'center' }}>点击立即刷新</Text>
+            </TouchableOpacity>
+        </View>;
+        let errorView = (_rePostFetch) => <View>
+            <Text style={{ textAlign: 'center' }}>网络请求失败</Text>
+            <TouchableOpacity
+
+                onPress={() => _rePostFetch()}
+            >
+                <Text style={{ color: '#333', textAlign: 'center' }}>点击立即刷新</Text>
+            </TouchableOpacity>
+        </View>;
+        let loading = () => <Text style={{ textAlign: 'center' }}>正在加载中。。。</Text>;
+        return {
+            footerLoading,
+            loadingNoMore,
+            loadingMore,
+            noDataView,
+            errorView,
+            loading
+        }
+    }
+
+    defaultTopIndicatorRender = (pulling, pullok, pullrelease, pullSuccess, gesturePosition) => {
+        // console.log(pulling, pullok, pullrelease, pullSuccess, gesturePosition)
+        this.transform = [{
+            rotate: this.state.prArrowDeg.interpolate({
+                inputRange: [0, 1],
+                outputRange: ['0deg', '-100deg']
+            })
+        }];
+        if (pulling) {
+            Animated.timing(this.state.prArrowDeg, {
+                toValue: 0,
+                duration: 100,
+                easing: Easing.inOut(Easing.quad)
+            }).start();
+        } else if (pullok) {
+            Animated.timing(this.state.prArrowDeg, {
+                toValue: 1,
+                duration: 100,
+                easing: Easing.inOut(Easing.quad)
+            }).start();
+        }
+        return (
+            <View style={[styles.headWrap]}>
+                <View ref={(c) => { this.txtPulling = c; }} style={pulling ? styles.show : styles.hide}>
+                    <Animated.Image style={[styles.arrow, { transform: this.transform }]}
+                                    resizeMode={'contain'}
+                                    source={{ uri: this.base64Icon }} />
+                    <Text style={styles.arrowText}>{"下拉可以刷新"}</Text>
+                </View>
+
+                <View ref={(c) => { this.txtPullok = c; }} style={pullok ? styles.show : styles.hide}>
+
+                    <Animated.Image style={[styles.arrow, { transform: this.transform }]}
+                                    resizeMode={'contain'}
+                                    source={{ uri: this.base64Icon }} />
+                    <Text style={styles.arrowText}>{"释放立即刷新"}</Text>
+                </View>
+
+                <View ref={(c) => { this.txtPullrelease = c; }} style={pullrelease ? styles.show : styles.hide}>
+                    <ActivityIndicator size="small" color="gray" style={styles.arrow} />
+                    <Text style={styles.arrowText}>{"刷新数据中..."}</Text>
+                </View>
+                <View ref={(c) => { this.txtPullSuccess = c; }} style={pullSuccess ? styles.show : styles.hide}>
+                    <Text style={styles.arrowText}>{"刷新成功.."}</Text>
+                </View>
+
+            </View>
+        );
+
+    };
 
 
     render() {
+
+
         return (
             <View style={styles.container}>
-                <FlatList
+                <SmartRefresh
+                    topBackgroundColor={'#fccb57'}
+                    initialNumToRender={7}
                     renderItem={this.renderMovieData}
-                    data={this.state.productData}
+                    initialPage={1}
+                    pageSize={10}
+                    AsycConnectedChange={'connectionChange'}
+                    onFetch={this.onFetch.bind(this)}
+                    topIndicatorRender={this.defaultTopIndicatorRender}
+                    {...this.footerView() }
                 />
+
             </View>
         );
     }
-
-
-    renderMovieData({item}) {
-        return (
-            <View style={styles.container}>
-
-                <View style={{
-                    width: Dimensions.get('window').width,
-                    padding:10,
-                    margin:10,
-                    backgroundColor: '#ffffff',
-                    shadowOpacity: 0.5,
-                    shadowRadius: 3,
-                    elevation: Platform.OS === "android" ? 3 : undefined,}}>
-
-                    <View>
-                        <Image style={styles.productImage}
-                               source={{uri: item.images.medium}}/>
-                    </View>
-
-                </View>
-            </View>
-
-
-
-        );
-    }
-
 
 }
 
@@ -111,12 +263,82 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
     },
 
+    cardStyle: {
+        padding: 5,
+        margin: 5,
+    },
+
     movieContainer: {
         flex: 1,
+        flexDirection: 'row',
+        paddingTop: 5,
+        paddingBottom: 5
     },
+
     productImage: {
         width: 100,
-        height: 150,
-        marginLeft: 10
+        height: 140,
+        marginLeft: 5,
+
+    },
+    movieContentContainer: {
+        flex: 1,
+        flexDirection: 'row',
+        marginLeft: 10,
+        justifyContent: 'flex-start',
+        alignItems: 'center',
+    },
+
+    contentLeft: {
+        flex: 3,
+        justifyContent: 'center',
+        alignItems: 'flex-start',
+    },
+
+    contentRight: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'flex-start',
+    },
+
+    movieTitle: {
+        fontSize: 16,
+        color: 'black',
+        fontWeight: 'bold',
+    },
+    footer: {
+        backgroundColor: '#f2f2f2',
+        justifyContent: 'center',
+        alignItems: 'center',
+        flexDirection: 'row',
+        height: 60
+    },
+    headWrap: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#eca02a',
+        height: 100,
+    },
+    arrow: {
+        height: 30,
+        width: 30,
+        marginRight: 20,
+    },
+    hide: {
+        position: 'absolute',
+        left: 10000
+    },
+    show: {
+        position: 'relative',
+        left: 0,
+        flexDirection: 'row',
+        width: Dimensions.get('window').width,
+        height: 40,
+        justifyContent: 'center',
+        alignItems: 'center',
+        zIndex: 999
+    },
+    arrowText: {
+        marginLeft: 20,
     },
 });
--- src/utils/CardView.js
+++ src/utils/CardView.js
@@ -1,65 +1,58 @@
-import React from 'react';
-import { Dimensions, Platform, Text, TouchableOpacity, View } from 'react-native';
 import PropTypes from 'prop-types';
+import React from 'react';
+import {
+    View,
+    Dimensions,
+    StyleSheet,
+    Platform,
+} from 'react-native';
 
-class CardView extends React.Component {
-    constructor(props) {
-        super(props)
-    }
+const CardView = props => {
+    const { children, elevation, opacity, cornerRadius } = props;
 
-    // TODO : Add shadow properties to icon - in process ♨
+    const cardStyle = Platform.select({
+        ios: () =>
+            StyleSheet.create({
+                container: {
+                    shadowRadius:elevation,
+                    shadowOpacity:opacity,
+                    shadowOffset:{ width: 0, height: elevation },
+                    borderRadius:cornerRadius,
+                    backgroundColor: props.backgroundColor,
+                    width: Dimensions.get('window').width - 40,
+                }
+            }),
+        android: () =>
+            StyleSheet.create({
+                container: {
+                    elevation:elevation,
+                    borderRadius:cornerRadius,
+                    backgroundColor: props.backgroundColor,
+                    width: Dimensions.get('window').width - 10,
+                }
+            })
+    })();
 
-    render() {
-        const container = {
-            shadowOffset : {
-                width : Platform.OS === "ios" ? this.props.style.shadowOffsetWidth : 0,
-                height: Platform.OS === "ios" ? this.props.style.shadowOffsetWidth : 0,
-            },
-            width        : this.props.style.width,
-            margin       : this.props.style.margin,
-            shadowColor  : this.props.style.shadowColor,
-            shadowOpacity: this.props.style.shadowOpacity,
-            shadowRadius : this.props.style.shadowRadius,
-            elevation    : Platform.OS === "android" ? this.props.elevation : undefined,
-        };
+    return(
+        <View style={[cardStyle.container, props.style]}>
+            {children}
+        </View>
+    )
 
-        return (
-            <View style={ container }>
-                <View style={ {
-                    backgroundColor: this.props.style.bgColor,
-                    overflow       : 'hidden',
-                    padding        : this.props.style.padding,
-                    width          : this.props.style.width,
-                    borderRadius   : this.props.borderRadius,
-                } }>
-                    <TouchableOpacity onPress={ this.props.onPress }>
-                        { this.props.children }
-                    </TouchableOpacity>
-                </View>
-            </View>
-        )
-    }
-}
+};
 
+CardView.prototype = {
+    backgroundColor: PropTypes.string,
+    elevation: PropTypes.number,
+    cornerRadius: PropTypes.number,
+    opacity: PropTypes.number
+};
 
 CardView.defaultProps = {
-    style: {
-        shadowColor       : '#000000',
-        shadowOffsetWidth : 3,
-        shadowOffsetHeight: 3,
-        shadowOpacity     : 0.1,
-        shadowRadius      : 3,
-        bgColor           : '#ffffff',
-        padding           : 5,
-        margin            : 10,
-        borderRadius      : 3,
-        width             : 300,
-        elevation         : 3,
-    },
-};
-CardView.propTypes = {
-    style       : PropTypes.object,
-    onPress     : PropTypes.func,
+    backgroundColor: '#ffffff',
+    elevation: 3,
+    cornerRadius: 5,
+    opacity: .5
 };
 
 export default CardView
